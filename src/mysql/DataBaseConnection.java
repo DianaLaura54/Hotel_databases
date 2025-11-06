4035618b977b8ba7acfa52fdb55fcf938a76ef9b
DianaLaura54 <101946736+DianaLaura54@users.noreply.github.com>
2025-11-06 13:10:13 +0200

    m
    m

--- src/mysql/DataBaseConnection.java
+++ src/mysql/DataBaseConnection.java
@@ -16,11 +16,11 @@ public class DataBaseConnection {
 
     public DataBaseConnection() {
         driver = "com.mysql.cj.jdbc.Driver";
-        connectionURL = "jdbc:mysql://localhost:3305/";
+        connectionURL = "jdbc:mysql://localhost:3306/";
         dbName = "mainn";
         ssl = "?autoReconnect=true&useSSL=false";
         username = "root";
-        password = "1969";
+        password = "Fizica1969*";
     }
 
     public static void main(String[] args) {
